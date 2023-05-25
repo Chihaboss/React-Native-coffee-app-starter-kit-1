a18d75fc01736650f88e17d4c9e9a6ce2cc79fb6
unknown <chihab.che@gmail.comgit config --global user.name>
2023-05-25 21:31:12 +0100

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import colors from "./app/config/colors";
+import HomeScreen from "./app/screens/HomeScreen";
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
+    <View style={{ flex:1, backgroundColor:colors.dark}}>
+      <HomeScreen></HomeScreen>
     </View>
   );
 };
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,11 +1,56 @@
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View, ScrollView, TouchableOpacity, Image } from "react-native";
 import React from "react";
+import SPACING from "../config/SPACING"
+import { BlurView } from "expo-blur";
+import {Ionicons} from "@expo/vector-icons"
+import colors from "../config/colors";
+
+const avatar = require("../../assets/avatar.jpg")
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        <View 
+        style={{
+          padding:  SPACING,
+          flexDirection : "row",
+          justifyContent : "space-between"
+        }}
+        >
+          <TouchableOpacity style={{
+            borderRadius: SPACING,
+            overflow: "hidden",
+            width: SPACING * 4,
+            height: SPACING * 4,
+            borderRadius : SPACING,
+
+          }}>
+            <BlurView style={{
+              height: "100%",
+              justifyContent: "center",
+              alignItems:"center",
+            }}>
+              <Ionicons name="menu"
+                        size={SPACING * 2.5}
+                        color={colors.secondary}
+              ></Ionicons>
+            </BlurView>
+          </TouchableOpacity>
+          <View>
+              <BlurView>
+                <Image style={{ 
+                  height: "100%", 
+                  width:"100%",
+                  borderRadius : SPACING * 2,  
+                }}
+                source={avatar}></Image>
+              </BlurView>
+          </View>
+
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
